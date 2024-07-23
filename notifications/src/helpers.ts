d078ba8f709953aaca7d5280d0e19748fd2dd42c
PC <crunchydeer30@gmail.com>
2024-07-23 12:36:17 +0300

    notifications: implement emailTemplates function

---
+++ notifications/src/helpers.ts
@@ -0,0 +1,52 @@
+import path from 'path';
+
+import { IEmailLocals, winstonLogger } from '@crunchydeer30/microservices-course-shared';
+import { config } from '@notifications/config';
+import nodemailer from 'nodemailer';
+import Email from 'email-templates';
+
+const logger = winstonLogger(`${config.ELASTIC_URL}`, 'mailTransportHelper', 'debug');
+
+export async function emailTemplates(template: string, receiver: string, locals: IEmailLocals): Promise<void> {
+  try {
+    const transport = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: config.SENDER_EMAIL,
+        pass: config.SENDER_EMAIL_PASSWORD,
+      },
+    });
+
+    const email: Email = new Email({
+      message: {
+        from: `Jobber App <${config.SENDER_EMAIL}>`,
+      },
+      send: true,
+      preview: false,
+      transport,
+      views: {
+        options: {
+          extension: 'ejs',
+        },
+      },
+      juice: true,
+      juiceResources: {
+        preserveImportant: true,
+        webResources: {
+          relativeTo: path.join(__dirname, '../build'),
+        },
+      },
+    });
+
+    await email.send({
+      template: path.join(__dirname, 'src/emails', template),
+      message: {
+        to: receiver,
+      },
+      locals,
+    });
+  } catch (err) {
+    logger.error(err);
+  }
+}
--- notifications/src/queues/mail.transport.ts
+++ notifications/src/queues/mail.transport.ts
@@ -1,10 +1,12 @@
 import { IEmailLocals, winstonLogger } from '@crunchydeer30/microservices-course-shared';
 import { config } from '@notifications/config';
+import { emailTemplates } from '@notifications/helpers';
 
 const logger = winstonLogger(`${config.ELASTIC_URL}`, 'mailTransport', 'debug');
 
 export async function sendEmail(template: string, receiver: string, locals: IEmailLocals): Promise<void> {
   try {
+    emailTemplates(template, receiver, locals);
     logger.info('Email sent succesfully');
   } catch (err) {
     logger.log('error', 'NotificationService MailTrasport sendEmail() error: ', err);
